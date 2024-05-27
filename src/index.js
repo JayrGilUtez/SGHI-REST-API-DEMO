3f52e8d5a4972c49a04a28368a1db1f70849f1e6
JayrGil <20223tn052@utez.edu.mx>
2024-05-27 15:46:33 -0500

    fix(config) enviroment variables fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import app from './app.js';
 import cors from 'cors';
-import { PORT } from './config.js';
+import { SERVER_PORT } from './config.js';
 
 app.use(cors());
-console.log('Server running on port', PORT);
-app.listen(PORT);
+console.log('Server running on port', SERVER_PORT);
+console.log(`http://localhost:${SERVER_PORT}/`);
+app.listen(SERVER_PORT);
