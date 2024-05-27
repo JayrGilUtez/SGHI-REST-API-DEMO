bf9738b005194c4390e317a15bd79055ecfe9602
JayrGil <20223tn052@utez.edu.mx>
2024-05-27 15:49:00 -0500

    feat(app) sotries routes added to app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import storiesRoutes from './routes/stories.routes.js';
 
 
 const app = express();
 app.use(express.json());
+app.use(storiesRoutes);
 
 export default app;
