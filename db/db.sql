a4ce9f8c23e4afda66b6b05cd6e13a095e5c43a4
JayrGil <20223tn052@utez.edu.mx>
2024-05-27 15:45:39 -0500

    feat(db) some inserts added to each table

--- db/db.sql
+++ db/db.sql
@@ -7,6 +7,13 @@ CREATE TABLE users (
     hasSubscription BOOLEAN DEFAULT FALSE
 );
 
+-- insert 3 users
+INSERT INTO users (name, hasSubscription) VALUES ('Jayr', TRUE);
+INSERT INTO users (name, hasSubscription) VALUES ('Nori', FALSE);
+INSERT INTO users (name, hasSubscription) VALUES ('Charlie', TRUE);
+
+
+
 CREATE TABLE stories(
     story_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
@@ -16,6 +23,15 @@ CREATE TABLE stories(
     diagram JSONB,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
+INSERT INTO stories (user_id, title, description, isPublic, diagram) 
+VALUES (1, 'Jayr s story', 'This is the Jayr s story description .', TRUE, '{}');
+
+INSERT INTO stories (user_id, title, description, isPublic, diagram) 
+VALUES (2, 'Nori s story', 'This is the Nori s story description .', TRUE, '{}');
+
+INSERT INTO stories (user_id, title, description, isPublic, diagram) 
+VALUES (3, 'Charlie s story', 'This is the Charlie s story description .', TRUE, '{}');
 
 
 CREATE TABLE scenes(
@@ -30,6 +46,11 @@ CREATE TABLE scenes(
     option_b INT NULL,
     FOREIGN KEY (story_id) REFERENCES stories(story_id)
 );
+
+
+
+INSERT INTO scenes (story_id, title, text_, image, video, audio, option_a, option_b) 
+VALUES (1, 'Scene Title', 'This is a description of the scene.', 'image.jpg', 'video.mp4', 'audio.mp3', 2, 3);
 
 SELECT * FROM users;
 SELECT * FROM stories;
