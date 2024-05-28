a73a2d8585ca765cd3d82ee3059c51e6fbfa420f
JayrGil <20223tn052@utez.edu.mx>
2024-05-28 13:13:15 -0500

    fix(imports) unused imports removed

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import { SERVER_PORT } from './config.js';
 
 app.use(cors());
 console.log('Server running on port', SERVER_PORT);
-console.log(`http://localhost:${SERVER_PORT}/`);
+console.log(`http://localhost:${SERVER_PORT}`);
+
 app.listen(SERVER_PORT);
