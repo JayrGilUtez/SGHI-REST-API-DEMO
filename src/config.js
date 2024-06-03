7fa4e11c6564ae3970dea304202b19da4a82db89
Galicia_Jimenez_Jayr_Gil <20223tn052@utez.edu.mx>
2024-06-03 08:08:33 -0600

    cassadra db configuration added

--- src/config.js
+++ src/config.js
@@ -9,3 +9,10 @@ export const DB_PORT = process.env.DB_PORT;
 export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB_NAME = process.env.DB_NAME;
+
+export const cassadraDBConfig = {
+    CONTACT_POINTS: process.env.CONTACT_POINTS,
+    CASSANDRA_DB_PORT: process.env.CASSANDRA_DB_PORT,
+    LOCAL_DATA_CENTER: process.env.LOCAL_DATA_CENTER,
+    KEYSPACE: process.env.KEYSPACE
+};
