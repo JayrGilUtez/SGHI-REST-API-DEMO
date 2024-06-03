66d917db52ebe4728d3c44256fcdddfa31603d95
Galicia_Jimenez_Jayr_Gil <20223tn052@utez.edu.mx>
2024-06-03 08:16:55 -0600

    images routes implemented

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import storiesRoutes from './routes/stories.routes.js';
+import imagesRoutes from './routes/images.routes.js'
 import cors from 'cors';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(storiesRoutes);
+app.use(imagesRoutes);
 
 export default app;
